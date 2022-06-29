163dc95244420f07d6a993393ba43f2e0fec3d2b
sidamakid <38231721+sidamakid@users.noreply.github.com>
2022-06-28 17:08:49 -0700

    Add files via upload

---
+++ Dr-Mouseover-Spells-Tables/Dr-Mouseover-Classic-Spells.lua
@@ -0,0 +1,67 @@
+local isClassicWow = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
+if isClassicWow then
+local L_DR_Mouse_Over_Text = DR_Mouse_Over_Test_Localization_My_Localization_Table
+Dr_Mouse_Over_Spells = {
+    --Incapacitates
+    ["Sap"] = L_DR_Mouse_Over_Text["Incapacitate"],
+    ["Polymorph"] = L_DR_Mouse_Over_Text["Incapacitate"],
+    ["Polymorph: Turtle"] = L_DR_Mouse_Over_Text["Incapacitate"],
+    ["Polymorph: Pig"] = L_DR_Mouse_Over_Text["Incapacitate"],
+    ["Repentance"] =  L_DR_Mouse_Over_Text["Incapacitate"],
+    ["Gouge"] = L_DR_Mouse_Over_Text["Incapacitate"],
+    ["Wyvern Sting"] = L_DR_Mouse_Over_Text["Incapacitate"],
+    --Kidney Shot
+    ["Kidney Shot"] = L_DR_Mouse_Over_Text["Kidney Shot"],
+    --Charm
+    ["Mind Control"] = L_DR_Mouse_Over_Text["Charm"],
+    --Scatter
+    ["Scatter Shot"] = L_DR_Mouse_Over_Text["Scatter Shot"],
+    -- Stun
+    ["Cheap Shot"] = L_DR_Mouse_Over_Text["Stun"],
+    ["Pounce"] = L_DR_Mouse_Over_Text["Stun"],
+    ["Bash"] = L_DR_Mouse_Over_Text["Stun"],
+    ["Hammer of Justice"] = L_DR_Mouse_Over_Text["Stun"],
+    ["Charge"] = L_DR_Mouse_Over_Text["Stun"],
+    ["Intercept"] = L_DR_Mouse_Over_Text["Stun"],
+    ["Concussion Blow"] = L_DR_Mouse_Over_Text["Stun"],
+    ["War stomp"] = L_DR_Mouse_Over_Text["Stun"],
+    ["Intimidation"] = L_DR_Mouse_Over_Text["Stun"],
+    --Stun Proc
+    ["Improved Starfire"] = L_DR_Mouse_Over_Text["Stun Proc"],
+    ["Impact"] = L_DR_Mouse_Over_Text["Stun Proc"],
+    ["Stoneclaw Totem"] = L_DR_Mouse_Over_Text["Stun Proc"],
+    ["Seal of Justice"] = L_DR_Mouse_Over_Text["Stun Proc"],
+    ["Blackout"] = L_DR_Mouse_Over_Text["Stun Proc"],
+    ["Mace Specialization"] = L_DR_Mouse_Over_Text["Stun Proc"],
+    --Sleep
+    ["Hibernate"] = L_DR_Mouse_Over_Text["Sleep"],
+    --Fear
+    ["Psychic Scream"] = L_DR_Mouse_Over_Text["Fear"],
+    ["Fear"] = L_DR_Mouse_Over_Text["Fear"],
+    ["Seduction"] = L_DR_Mouse_Over_Text["Fear"],
+    ["Howl of Terror"] = L_DR_Mouse_Over_Text["Fear"],
+    ["Intimidating Shout"] = L_DR_Mouse_Over_Text["Fear"],
+    ["Scare Beast"] = L_DR_Mouse_Over_Text["Fear"],
+    ["Turn Evil"] = L_DR_Mouse_Over_Text["Fear"],
+    --Horror
+    ["Death Coil"] = L_DR_Mouse_Over_Text["None"],
+    --Counterattack
+    ["Counterattack"] = L_DR_Mouse_Over_Text["Counterattack"],
+    --Root
+    ["Entangling Roots"] = L_DR_Mouse_Over_Text["Root"],
+    ["Frost Nova"] = L_DR_Mouse_Over_Text["Root"],
+    ["Nature's Grasp"] = L_DR_Mouse_Over_Text["Root"],
+    --Disorient
+    ["Blind"] = L_DR_Mouse_Over_Text["None"],
+    --Root Proc
+    ["Frostbite"] = L_DR_Mouse_Over_Text["Root Proc"],
+    ["Improved Hamstring"] = L_DR_Mouse_Over_Text["Root Proc"],
+    ["Improved Wing Clip"] = L_DR_Mouse_Over_Text["Root Proc"],
+    ["Entrapment"] = L_DR_Mouse_Over_Text["Root Proc"],
+    --Disarm
+    ["Disarm"] = L_DR_Mouse_Over_Text["None"],
+    ["Riposte"] = L_DR_Mouse_Over_Text["None"],
+    --Frost Shock
+    ["Frost Shock"] = L_DR_Mouse_Over_Text["Frost Shock"],
+}
+end
