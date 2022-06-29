fe1c605779beed31624d5d663f6cf3276fee8820
sidamakid <38231721+sidamakid@users.noreply.github.com>
2022-06-28 17:08:24 -0700

    Add files via upload

---
+++ Localizing/Dr-Mouseover-localize-Text-ruRU.lua
@@ -0,0 +1,30 @@
+local locale = GetLocale()
+local L_DR_Mouse_Over_Text = DR_Mouse_Over_Test_Localization_My_Localization_Table
+
+if locale == "ruRU" then
+    --Russian
+    L_DR_Mouse_Over_Text["Kidney Shot"] = "Выстрел в почку";
+    L_DR_Mouse_Over_Text["Dr-Category"] = "Dr-Категория";
+    L_DR_Mouse_Over_Text["Stun"] = "Оглушать";
+    L_DR_Mouse_Over_Text["Stun Proc"] = "Оглушающий прок";
+    L_DR_Mouse_Over_Text["Incapacitate"] = "Делать неспособным";
+    L_DR_Mouse_Over_Text["Fear"] = "Страх";
+    L_DR_Mouse_Over_Text["None"] = "Никакой";
+    L_DR_Mouse_Over_Text["Disorient"] = "Дезориентировать";
+    L_DR_Mouse_Over_Text["Charm"] = "Обаяние";
+    L_DR_Mouse_Over_Text["Scatter Shot / Dragon's Breath"] = "Скаттер-шот / Дыхание дракона";
+    L_DR_Mouse_Over_Text["Disarm"] = "Обезоруживать";
+    L_DR_Mouse_Over_Text["Silence"] = "Тишина";
+    L_DR_Mouse_Over_Text["Root"] = "Корень";
+    L_DR_Mouse_Over_Text["Root Proc"] = "Корневой процесс";
+    L_DR_Mouse_Over_Text["Counterattack"] = "Контратака";
+    L_DR_Mouse_Over_Text["Sleep"] = "Спать";
+    L_DR_Mouse_Over_Text["Unstable Affliction"] = "Нестабильный недуг";
+    L_DR_Mouse_Over_Text["Chastise"] = "Карать";
+    L_DR_Mouse_Over_Text["Horror"] = "Ужас";
+    L_DR_Mouse_Over_Text["Opener Stun"] = "Оглушение открывалки";
+    L_DR_Mouse_Over_Text["Knockback"] = "Нокбэк";
+    L_DR_Mouse_Over_Text["Taunt"] = "Насмешка";
+    L_DR_Mouse_Over_Text["Frost Shock"] = "Морозный шок";
+    L_DR_Mouse_Over_Text["Scatter Shot"] = "Разброс выстрела";
+end
