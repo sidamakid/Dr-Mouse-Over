64d33fb25821a7b0df024e184f051781be3c1e1f
sidamakid <amsalu@beestung.com>
2022-07-05 19:30:28 -0700

    Updated 7/05/22

---
+++ Dr-Mouseover/Localizing/Dr-Mouseover-localize-Text-esMX.lua
@@ -0,0 +1,30 @@
+local locale = GetLocale()
+local L_DR_Mouse_Over_Text = DR_Mouse_Over_Test_Localization_My_Localization_Table
+
+if locale == "esMX" then
+    --Spanish
+    L_DR_Mouse_Over_Text["Kidney Shot"] = "Inyección de riñón";
+    L_DR_Mouse_Over_Text["Dr-Category"] = "Categoría Dr";
+    L_DR_Mouse_Over_Text["Stun"] = "Aturdir";
+    L_DR_Mouse_Over_Text["Stun Proc"] = "Aturdir a Proc";
+    L_DR_Mouse_Over_Text["Incapacitate"] = "Incapacitar";
+    L_DR_Mouse_Over_Text["Fear"] = "Miedo";
+    L_DR_Mouse_Over_Text["None"] = "Ninguno";
+    L_DR_Mouse_Over_Text["Disorient"] = "Desorientar";
+    L_DR_Mouse_Over_Text["Charm"] = "Encanto";
+    L_DR_Mouse_Over_Text["Scatter Shot / Dragon's Breath"] = "Scatter Shot / Aliento de dragón";
+    L_DR_Mouse_Over_Text["Disarm"] = "Desarmar";
+    L_DR_Mouse_Over_Text["Silence"] = "Silencio";
+    L_DR_Mouse_Over_Text["Root"] = "Raíz";
+    L_DR_Mouse_Over_Text["Root Proc"] = "Raíz Proc";
+    L_DR_Mouse_Over_Text["Counterattack"] = "Contraataque";
+    L_DR_Mouse_Over_Text["Sleep"] = "Dormir";
+    L_DR_Mouse_Over_Text["Unstable Affliction"] = "Aflicción inestable";
+    L_DR_Mouse_Over_Text["Chastise"] = "Castigar";
+    L_DR_Mouse_Over_Text["Horror"] = "Horror";
+    L_DR_Mouse_Over_Text["Opener Stun"] = "Aturdimiento de apertura";
+    L_DR_Mouse_Over_Text["Knockback"] = "Retroceso";
+    L_DR_Mouse_Over_Text["Taunt"] = "Mofa";
+    L_DR_Mouse_Over_Text["Frost Shock"] = "Choque de heladas";
+    L_DR_Mouse_Over_Text["Scatter Shot"] = "Disparo de dispersión";
+end
---
+++ Dr-Mouseover/Localizing/Dr-Mouseover-localize-Text-zhCN.lua
@@ -0,0 +1,30 @@
+local locale = GetLocale()
+local L_DR_Mouse_Over_Text = DR_Mouse_Over_Test_Localization_My_Localization_Table
+
+if locale == "zhCN" then
+    --Chinese simplifed
+    L_DR_Mouse_Over_Text["Kidney Shot"] = "肾脏注射";
+    L_DR_Mouse_Over_Text["Dr-Category"] = "博士类";
+    L_DR_Mouse_Over_Text["Stun"] = "打 晕";
+    L_DR_Mouse_Over_Text["Stun Proc"] = "眩晕过程";
+    L_DR_Mouse_Over_Text["Incapacitate"] = "失能";
+    L_DR_Mouse_Over_Text["Fear"] = "恐惧";
+    L_DR_Mouse_Over_Text["None"] = "没有";
+    L_DR_Mouse_Over_Text["Disorient"] = "迷失方向";
+    L_DR_Mouse_Over_Text["Charm"] = "魅力";
+    L_DR_Mouse_Over_Text["Scatter Shot / Dragon's Breath"] = "散射/龙的呼吸";
+    L_DR_Mouse_Over_Text["Disarm"] = "缴械";
+    L_DR_Mouse_Over_Text["Silence"] = "沉默";
+    L_DR_Mouse_Over_Text["Root"] =  "根";
+    L_DR_Mouse_Over_Text["Root Proc"] = "根过程";
+    L_DR_Mouse_Over_Text["Counterattack"] = "反攻";
+    L_DR_Mouse_Over_Text["Sleep"] = "睡";
+    L_DR_Mouse_Over_Text["Unstable Affliction"] = "不稳定的折磨";
+    L_DR_Mouse_Over_Text["Chastise"] = "惩罚";
+    L_DR_Mouse_Over_Text["Horror"] = "恐怖";
+    L_DR_Mouse_Over_Text["Opener Stun"] = "打开程序眩晕";
+    L_DR_Mouse_Over_Text["Knockback"] = "击退";
+    L_DR_Mouse_Over_Text["Taunt"] = "嘲讽";
+    L_DR_Mouse_Over_Text["Frost Shock"] = "霜冻冲击";
+    L_DR_Mouse_Over_Text["Scatter Shot"] = "散射";
+end
